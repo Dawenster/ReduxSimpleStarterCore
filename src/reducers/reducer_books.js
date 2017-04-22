74c9aedb4230bbae61c8dd652e48bec7d9cdd206
David Wen <dstwen@gmail.com>
2017-04-22 14:02:46 -0700

    Finish book app

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript: The Good Parts', pages: 30 },
+    { title: 'Harry Potter', pages: 123 },
+    { title: 'The Dark Tower', pages: 32 },
+    { title: 'Eloquent Ruby', pages: 1 }
+  ]
+}
